f419173988b3908160b0f459ef73c3975c9dc282
nathan-mccraw <naterltw175@gmail.com>
2021-07-12 23:21:47 -0400

    Add Home formatting.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,37 @@
 const Home = () => {
-  return <div>HOME</div>;
+  return (
+    <div className="container">
+      <div className="col align-items-center justify-content-center mt-4">
+        <div className="row">
+          <div className="border border-info border-2 p-4 shadow-lg text-center">
+            Information{" "}
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              molestiae et aperiam sit. Quos error explicabo blanditiis labore
+              laborum at porro architecto quisquam! Quia perspiciatis omnis iste
+              natus nemo porro!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis ullam fuga praesentium eligendi dolorum accusantium
+              vitae recusandae, unde blanditiis consequuntur non earum, dolore
+              libero veritatis quae ratione! Rem, mollitia esse.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+              ratione repellendus eum molestiae placeat magni numquam, libero,
+              perferendis totam expedita rerum nihil quaerat reiciendis tempore,
+              facere in esse minus dolorem?
+            </p>
+          </div>
+        </div>
+        <div className="row justify-content-center text-center mt-3">
+          <div className="col">How To Use 'Search'</div>
+          <div className="col">How To Use 'Favorites'</div>
+          <div className="col">Info</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Home;
--- src/Info.js
+++ src/Info.js
@@ -1,5 +1,44 @@
+import { NavLink } from "react-router-dom";
+
 const Info = () => {
-  return <div>INFO</div>;
+  return (
+    <div className="container ms-1 me-2">
+      <div className="row justify-content-start">
+        <div className="col-2">
+          <NavLink
+            className="nav-link active activeLink"
+            aria-current="page"
+            exact
+            to="/SearchResults"
+          >
+            Back to Search Results
+          </NavLink>
+          Info List
+        </div>
+        <div className="col-10">
+          Information{" "}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+            molestiae et aperiam sit. Quos error explicabo blanditiis labore
+            laborum at porro architecto quisquam! Quia perspiciatis omnis iste
+            natus nemo porro!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis ullam fuga praesentium eligendi dolorum accusantium
+            vitae recusandae, unde blanditiis consequuntur non earum, dolore
+            libero veritatis quae ratione! Rem, mollitia esse.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            ratione repellendus eum molestiae placeat magni numquam, libero,
+            perferendis totam expedita rerum nihil quaerat reiciendis tempore,
+            facere in esse minus dolorem?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
